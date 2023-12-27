4387db02a7587424661a99477e26cbf46a6ed7d9
Luis-Arnold <60340495+Luis-Arnold@users.noreply.github.com>
2023-12-27 15:16:57 +0100

    Revert "X Problems"
    This reverts commit 3c444b276e425507f2f40ebb745c2a6632330b41.

--- Content/Scenes/Soldier/Soldier3D.gd
+++ Content/Scenes/Soldier/Soldier3D.gd
@@ -1,8 +1,47 @@
-extends CharacterBody3D
+class_name Soldier3D extends CharacterBody3D
 
+@export_category('Core')
 var playerColor: PlayerUtil.playerColor
-const SPEED = 1.0
+var moral: int
+# Experience when killing
+var experience: int
+# Walking slower and attacking slower
+var exhaustion: int
 
+@export_category('Movement')
+var currentSpeed: int = 1.0
+var destination: Vector2 = Vector2()
+var formationPosition: Vector2 # Relative position in the formation
+var stamina: int = 100
+
+@export_category('Combat')
+var health: int = 100
+var armor: int
+var attackReady: bool = false
+# Grows with experience
+var attackSpeed: float = 1
+var attackCooldown: int
+var attackDamage: int
+var waysOfAttack
+# Grows with experience
+var accuracy: float
+# What you are resistant or not resistant against
+var resistanceModifiers: Dictionary
+var targetsInRange: Array = []
+signal soldierDamaged
+signal soldierDied
+
+@export_category('Selection')
+var selected: bool = false
+signal soldierSelected
+signal soldierDeselected
+
+var currentUnit: Unit3D
+
+var currentTile: TileData
+signal tileChanged
+
+@export_category('Helper variables')
 var nodesReady: bool = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -14,7 +53,10 @@ func _ready():
 	add_to_group('soldier')
 	call_deferred('onNodesReady')
 	
+	
 	%NavAgent.target_position = position
+	connect('soldierSelected', Callable(self, 'onSelected'))
+	connect('soldierDeselected', Callable(self, 'onDeselected'))
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -43,14 +85,18 @@ func _physics_process(delta):
 	if nodesReady:
 		if %NavAgent.distance_to_target() > %NavAgent.target_desired_distance:
 			var nextPosition = %NavAgent.get_next_path_position()
-			velocity = (nextPosition - position).normalized() * SPEED
+			velocity = (nextPosition - position).normalized() * currentSpeed
 			move_and_slide()
 
 func select():
-	pass
+	if not currentUnit.isSelected:
+		emit_signal('soldierSelected')
+		currentUnit.select()
 
 func deselect():
-	pass
+	if currentUnit.isSelected:
+		emit_signal('soldierDeselected')
+		currentUnit.deselect()
 
 func getScreenPosition(camera: Camera3D) -> Vector2:
 	return camera.unproject_position(position * camera.size)
@@ -73,4 +119,13 @@ func setTargetPosition(targetPosition):
 	%NavAgent.target_position = targetPosition
 
 func _on_nav_agent_target_reached():
+	pass
+
+func rotateSoldier(rotation: float):
+	%DirectionIndicator.rotation.z = rotation
+
+func onSelected():
+	pass
+
+func onDeselected():
 	pass
